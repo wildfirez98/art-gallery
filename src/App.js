b52076b55737cef4a72ac3316e72731464401c18
Jamie <james@areafamily.us>
2022-03-23 19:56:24 -0500

    First commit - gallery is up with pictures!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Gallery from './Gallery';
+import ButtonBar from './ButtonBar';
+import {useState, useEffect} from 'react'
 
 function App() {
+
+  let [artId, setArtId] = useState(12720)
+  let [data, setData] = useState({})
+
+  const handleIterate = (e) => {
+  setArtId(artId + Number(e.target.value))
+  }
+  
+  useEffect(() => {
+      document.title="Welcome to Artworld"
+      fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${artId}`)
+      .then(response => response.json())
+      .then(resData => setData(resData))
+  }, [artId])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Gallery objectImg={data.primaryImage} artist={data.artistDisplayName} title={data.title} />
+      <ButtonBar handleIterate={handleIterate} />
     </div>
   );
 }
---
+++ src/Gallery.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Gallery(props){
+    return (
+        <div style={{'width': '100%'}}>
+            <img src={props.objectImg} alt={props.title} />
+            <p>{props.artist}</p>
+        </div>
+    )
+}
+
+export default Gallery
